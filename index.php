078eea07599c8ec98d61473d3e5820459d462473
CWSTeam <michael@cwsgaming.com>
2014-02-27 17:05:25 -0500

    dropdown

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
+<?php session_start(); ?>
+	
 	<!DOCTYPE html>
-	<html lang="en">
+	<html>
 	  <head>
 	    <meta charset="utf-8">
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -34,7 +36,7 @@
 	    <!-- Collect the nav links, forms, and other content for toggling -->
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	      <ul class="nav navbar-nav">
-	        <li class="active"><a href="#">Home</a></li>
+	        <li class="active"><a href="index.php">Home</a></li>
 	        <!-- <li><a href="#">Link</a></li> -->
 	        <li class="dropdown">
 	          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Edge <b class="caret"></b></a>
@@ -47,24 +49,9 @@
 	          </ul>
 	        </li>
 	      </ul>
-	      <form class="navbar-form navbar-left" role="search">
-	        <div class="form-group">
-	          <input type="text" class="form-control" placeholder="Search">
-	        </div>
-	        <button type="submit" class="btn btn-default">Submit</button>
-	      </form>
+	      
 	      <ul class="nav navbar-nav navbar-right">
-	        <li><a href="#">Link</a></li>
-	        <li class="dropdown">
-	          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-	          <ul class="dropdown-menu">
-	            <li><a href="#">Action</a></li>
-	            <li><a href="#">Another action</a></li>
-	            <li><a href="#">Something else here</a></li>
-	            <li class="divider"></li>
-	            <li><a href="#">Separated link</a></li>
-	          </ul>
-	        </li>
+	      	<li><a href="register.php">Register</a></li>
 	      </ul>
 	    </div><!-- /.navbar-collapse -->
 	  </div><!-- /.container-fluid -->
@@ -73,7 +60,7 @@
 		
 		<div class="container-fluid">
 		
-		<div class="row col-xs-2" id="left-sidebar" onmouseover="changeSidebarLeftColorBlue()"">
+		<div class="row col-xs-2" id="left-sidebar" onmouseover="changeSidebarLeftColorBlue()">
 				<p>hi</p>
 		</div>		
 				
---
+++ register.php
@@ -0,0 +1,101 @@
+	<?php session_start(); ?>
+		
+		<!DOCTYPE html>
+		<html>
+		  <head>
+		    <meta charset="utf-8">
+		    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		    <meta name="viewport" content="width=device-width, initial-scale=1">
+		    <title>Bootstrap 101 Template</title>
+		
+		    <!-- Bootstrap -->
+		    <link href="css/bootstrap.css" rel="stylesheet">
+		    <link href="css/custom.css" rel="stylesheet">
+		
+		    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		    <!--[if lt IE 9]>
+		      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		    <![endif]-->
+		  </head>
+		  <body background="images/bckg.png">
+		    <nav class="navbar navbar-default" role="navigation">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">Binum</a>
+		    </div>
+		
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li><a href="index.php">Home</a></li>
+		        <!-- <li><a href="#">Link</a></li> -->
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Edge <b class="caret"></b></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="#">Edge Console</a></li>
+		            <li><a href="#">Edge Phone</a></li>
+		            <li><a href="#">Something else here</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#">Support</a></li>
+		          </ul>
+		        </li>
+		      </ul>
+		      <ul class="nav navbar-nav navbar-right">
+		        <li class="active"><a href="register.php"> Register </a></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+		
+			
+			<div class="container-fluid">
+			
+			<div class="row col-xs-12" id="left-sidebar">
+					<form action="index.php" method="post" class="registration_form">
+	  <fieldset>
+	    <legend>Registration Form </legend>
+	
+	    <p>Create A new Account <span style="background:#EAEAEA none repeat scroll 0 0;line-height:1;margin-left:210px;;padding:5px 7px;">
+	Already a member? <a href="login.php">Log in</a></span> </p>
+	
+	    <div class="elements">
+	      <label for="name">Name :</label>
+	      <input type="text" class="form-control" id="name" name="name" size="25" />
+	    </div>
+	    <div class="elements">
+	      <label for="e-mail">E-mail :</label>
+	      <input type="text" class="form-control" id="e-mail" name="e-mail" size="25" />
+	    </div>
+	    <div class="elements">
+	      <label for="Password">Password:</label>
+	      <input type="password" class="form-control" id="Password" name="Password" size="25" />
+	    </div>
+	    <div class="submit">
+	    <br />
+	     <input type="hidden" name="formsubmitted" value="TRUE" />
+	      <input type="submit" class="btn btn-primary" value="Register" />
+	    </div>
+	  </fieldset>
+	</form>
+	
+			</div>		
+					
+			</div>
+		
+		    <!-- jQuery (necessary for Bootstraps JavaScript plugins) -->
+		    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		    <!-- Include all compiled plugins (below), or include individual files as needed -->
+		    <script src="js/bootstrap.js"></script>
+		    <!-- Native libraries -->
+		    <script src="js/binum.js"></script>
+		  </body>
+		</html>
