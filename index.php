27421eb6271efbc3d5afba4ce685f4c6dd0972ec
CWSTeam <michael@cwsgaming.com>
2014-03-01 21:39:52 -0500

    recolored menu. Working on making a nice body and placing menu in a more attractive place

--- index.php
+++ index.php
@@ -20,51 +20,46 @@
 	    <![endif]-->
 	  </head>
 	  <body background="images/bckg.png">
-	    <nav class="navbar navbar-default" role="navigation">
-	  <div class="container-fluid">
-	    <!-- Brand and toggle get grouped for better mobile display -->
-	    <div class="navbar-header">
-	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	      <a class="navbar-brand" href="#">Binum</a>
-	    </div>
-	
-	    <!-- Collect the nav links, forms, and other content for toggling -->
-	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	      <ul class="nav navbar-nav">
-	        <li class="active"><a href="index.php">Home</a></li>
-	        <!-- <li><a href="#">Link</a></li> -->
-	        <li class="dropdown">
-	          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Edge <b class="caret"></b></a>
-	          <ul class="dropdown-menu">
-	            <li><a href="#">Edge Console</a></li>
-	            <li><a href="#">Edge Phone</a></li>
-	            <li><a href="#">Something else here</a></li>
-	            <li class="divider"></li>
-	            <li><a href="#">Support</a></li>
-	          </ul>
-	        </li>
-	      </ul>
-	      
-	      <ul class="nav navbar-nav navbar-right">
-	      	<li><a href="register.php">Register</a></li>
-	      </ul>
-	    </div><!-- /.navbar-collapse -->
-	  </div><!-- /.container-fluid -->
-	</nav>
+		  <nav class="navbar navbar-inverse" role="navigation">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">Binum</a>
+		    </div>
+		
+		    <!-- Collect the nav links, forms, and other content for toggling
+				bs-example-navbar-collapse-1
+				#bs-example-navbar=collapse-1		    
+		     -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a href="index.php">Home</a></li>
+		        <!-- <li><a href="#">Link</a></li> -->
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Edge <b class="caret"></b></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="#">Edge Console</a></li>
+		            <li><a href="#">Edge Phone</a></li>
+		            <li><a href="#">Something else here</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#">Support</a></li>
+		          </ul>
+		        </li>
+		      </ul>
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="register.php"> Register </a></li>
+		      </ul>
+		    </div> <!-- /.navbar-collapse -->
+		  </div> <!-- /.container-fluid -->
+		</nav>
 	
 		
-		<div class="container-fluid">
-		
-		<div class="row col-xs-2" id="left-sidebar" onmouseover="changeSidebarLeftColorBlue()">
-				<p>hi</p>
-		</div>		
-				
-		</div>
 	
 	    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
--- register.php
+++ register.php
@@ -74,7 +74,7 @@ Yay! It worked!
 		    <![endif]-->
 		  </head>
 		  <body background="images/bckg.png">
-		  <nav class="navbar navbar-default" role="navigation">
+		  <nav class="navbar navbar-inverse" role="navigation">
 		  <div class="container-fluid">
 		    <!-- Brand and toggle get grouped for better mobile display -->
 		    <div class="navbar-header">
